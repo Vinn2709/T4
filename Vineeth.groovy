64aea3556a9c4247441368a606972a5e280c2677
Vinn2709 <129804565+Vinn2709@users.noreply.github.com>
2024-02-09 16:51:33 -0500

    Add files via upload

---
+++ Vineeth.groovy
@@ -0,0 +1,25 @@
+pipeline {
+    agent any
+    tools {
+        jdk 'JDK17'
+        maven 'maven3'
+    }
+
+    stages {
+        stage('Git Checkout') {
+            steps {
+                git branch: 'main', url: 'https://github.com/AnilkumarMaguluri18/Petclinic.git'
+            }
+        }
+        stage('Code Compile') {
+            steps {
+                bat label: 'Compile', script: 'mvn clean compile'
+            }
+        }
+        stage('Unit Tests') {
+            steps {
+                bat label: 'Run Tests', script: 'mvn test'
+            }
+        }
+     }
+  }
